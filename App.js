731cff46829c34bde4aa53bd9159452c3be0f191
sadegh <akbarisadegh@gmail.com>
2022-11-15 14:33:09 +0330

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={tw`text-red-300`}>Open up App.js to start working on reza!</Text>
       <StatusBar style="auto" />
     </View>
   );
