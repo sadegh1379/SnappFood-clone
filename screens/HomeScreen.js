680c5a64e856ec174ed293e888c9efe00c15717e
sadegh <akbarisadegh@gmail.com>
2022-11-15 18:45:00 +0330

    feat/header-top

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,9 @@
 import { useNavigation } from '@react-navigation/native'
 import React, { useLayoutEffect } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+import tw from 'twrnc'
+import { ChevronDownIcon, UserIcon } from 'react-native-heroicons/solid'
+
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -11,7 +14,22 @@ const HomeScreen = () => {
   }, [])
   return (
     <View>
-      <Text>HomeScreen</Text>
+      {/* header top */}
+      <View style={tw`bg-white p-2 flex flex-row items-center justify-between`}>
+          {/* title */}
+          <View style={tw`flex flex-row items-center`}>
+               <Image style={tw`h-10 w-10 rounded-full`} source={require('../assets/my-profile.jpg')}/>
+               <View style={tw`ml-3`}>
+                    <Text style={tw`text-xs text-gray-300`}>Deliver Noew!</Text>
+                    <View style={tw`flex flex-row items-center`}>
+                         <Text style={tw`text-xl font-bold`}>Current Location</Text>
+                         <ChevronDownIcon style={tw`mt-1`} size={25} color="#00CCBB"/>
+                    </View>
+               </View>
+          </View>
+          {/* user avatar */}
+          <UserIcon size={30} color="#00CCBB"/>
+      </View>
     </View>
   )
 }
